efac00f7859d7e27d32bef8a79ac13f1ca7ffe2f
Eugene <eugene2220202111@gmail.com>
2025-06-16 03:26:02 +0300

    Додані файли конфігурації для HTML, CSS та js

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+// eslint.config.js
+export default [
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+        },
+        rules: {
+            semi: ['error', 'always'],
+            quotes: ['error', 'double'],
+            'no-unused-vars': 'warn',
+            'no-console': 'off',
+        },
+    },
+];
